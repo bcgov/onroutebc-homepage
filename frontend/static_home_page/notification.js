be373976373affb46af9766f182d09b59f4ef4db
GlenAOT <160973940+GlenAOT@users.noreply.github.com>
2024-12-20 14:41:40 -0800

    homepage updates

---
+++ frontend/static_home_page/notification.js
@@ -0,0 +1,33 @@
+// Notification
+const notification = {
+  // true | false (no quoation marks)
+  show: true,
+  // "red" | "blue" | "green" | "gold"
+  color: "red",
+  // "Your header text here"
+  header: "Important Notice",
+  // "Your content text here" (tip: insert "\n" for new line and "\t" for tabs)
+  content: "onRouteBC is undergoing scheduled maintenance on [Date] from [Start Time] to [End Time]. During this period, you may experience temporary disruptions. We apologize for any inconvenience and appreciate your understanding."
+};
+
+
+// Script
+const notificationContainer = document.querySelector("#notification");
+
+const notificationSection = document.createElement("div");
+notificationSection.className = "box notification"
+
+const header = document.createElement("div");
+header.className = `box-header notification__header notification__header--${notification.color}`;
+header.innerHTML = `<h2>${notification.header}</h2>`;
+
+const content = document.createElement("div");
+content.className = `box-content notification__content notification__content--${notification.color}`;
+content.innerHTML = `<p>${notification.content}</p>`;
+
+notificationSection.appendChild(header)
+notificationSection.appendChild(content)
+
+if (notificationContainer && notification.show) {
+  notificationContainer.appendChild(notificationSection);
+}
